0a61c1475ed09e9b89472c219c71e95d4e96c9e9
ibarbylev <ibarbylev@gmail.com>
2023-09-24 03:45:19 +0300

    first commit'

---
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.taskmanager.ui.theme.TaskManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TaskManagerApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskManagerApp(modifier: Modifier = Modifier) {
+    TaskManager()
+}
+
+@Composable
+fun TaskManager(name: String = "Android", modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TaskManagerTheme {
+        TaskManagerApp()
+    }
+}
