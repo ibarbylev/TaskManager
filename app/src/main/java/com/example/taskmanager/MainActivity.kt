1c71bc6fd1050e52cd660a6ed3497bdc26041382
ibarbylev <ibarbylev@gmail.com>
2023-09-25 00:05:27 +0300

    completed work

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,15 +45,34 @@ fun TaskManagerApp(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun TaskManager(name: String = "Android", modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TaskManager(name: String = "Android",
+                painter: Painter = painterResource(R.drawable.ic_task_completed),
+                comment: String = "All tasks completed",
+                msg: String = "Nice work!",
+                modifier: Modifier = Modifier) {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painter,
+            contentDescription = null,
+        )
+        Text(
+            text = comment,
+            modifier = modifier.padding(top = 24.dp, bottom = 8.dp),
+            fontWeight = FontWeight.Bold,
+        )
+        Text(
+            text = msg,
+            modifier = modifier,
+            fontSize = 16.sp,
+        )
+    }
 
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     TaskManagerTheme {
